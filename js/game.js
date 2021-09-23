7d16d6a06d81cae3e64ed4f52d93447bffffe0cc
ValentinDouvry <valentin.jabba@gmail.com>
2021-09-23 13:31:17 +0400

    visuel plateau final

--- js/game.js
+++ js/game.js
@@ -5,30 +5,33 @@ let tileW = 100; // Largeur des cases
 let mapW = 10; // Nombre de cases par ligne
 let mapH = 7; // Nombre de ligne
 
+const playerIconH = 25;
+const playerIconW = 25;
+
 canvasGame = document.getElementById('gameCanvas');
 ctx = canvasGame.getContext('2d')
 scene = new createjs.Stage(canvasGame);
 
 const startPos = [
   {
-    x: 30,
+    x: 30, // Player 1
     y: 30
   },
   {
-    x: 70,
+    x: 70, // Player 2
     y: 30
   },
   {
-    x: 30,
+    x: 30, // Player 3
     y: 70
   },
   {
-    x: 70,
+    x: 70, // Player 4
     y: 70
   }
 ];
 
-let players = [
+const players = [
   {
     id: 0,
     name: "Joueur1",
@@ -36,35 +39,55 @@ let players = [
   {
     id: 1,
     name: "Joueur2",
+  },
+  {
+    id: 2,
+    name: "Joueur3",
+  },
+  {
+    id: 3,
+    name: "Joueur4",
   }
 ];
 
 let actualPlayerId = players[0].id;
 
 // Creation des pions des joueurs
-const j1Point = new createjs.Shape();
-j1Point.graphics.beginFill("Blue").drawCircle(0, 0, 10);
-j1Point.x = startPos[0].x;
-j1Point.y = startPos[0].y;
-scene.addChild(j1Point);
+const imageJ1 = new Image(playerIconW, playerIconH);
+imageJ1.src = './assets/pawnJ1.png';
+const bitmapJ1 = new createjs.Bitmap(imageJ1);
+bitmapJ1.regX = 12.5;
+bitmapJ1.regY = 12.5;
+bitmapJ1.x = startPos[0].x;
+bitmapJ1.y = startPos[0].y;
 
-const j2Point = new createjs.Shape();
-j2Point.graphics.beginFill("Red").drawCircle(0, 0, 10);
-j2Point.x = startPos[1].x;
-j2Point.y = startPos[1].y;
+const imageJ2 = new Image(playerIconW, playerIconH);
+imageJ2.src = './assets/pawnJ2.png';
+const bitmapJ2 = new createjs.Bitmap(imageJ2);
+bitmapJ2.regX = 12.5;
+bitmapJ2.regY = 12.5;
+bitmapJ2.x = startPos[1].x;
+bitmapJ2.y = startPos[1].y;
 
-const j3Point = new createjs.Shape();
-j3Point.graphics.beginFill("Green").drawCircle(0, 0, 10);
-j3Point.x = startPos[2].x;
-j3Point.y = startPos[2].y;
+const imageJ3 = new Image(playerIconW, playerIconH);
+imageJ3.src = './assets/pawnJ3.png';
+const bitmapJ3 = new createjs.Bitmap(imageJ3);
+bitmapJ3.regX = 12.5;
+bitmapJ3.regY = 12.5;
+bitmapJ3.x = startPos[2].x;
+bitmapJ3.y = startPos[2].y;
 
-const j4Point = new createjs.Shape();
-j4Point.graphics.beginFill("Yellow").drawCircle(0, 0, 10);
-j4Point.x = startPos[3].x;
-j4Point.y = startPos[3].y;
+const imageJ4 = new Image(playerIconW, playerIconH);
+imageJ4.src = './assets/pawnJ4.png';
+const bitmapJ4 = new createjs.Bitmap(imageJ4);
+bitmapJ4.regX = 12.5;
+bitmapJ4.regY = 12.5;
+bitmapJ4.x = startPos[3].x;
+bitmapJ4.y = startPos[3].y;
 
 const playersPoints = [];
 
+const tiles = [];
 
 //CASES
 const start = 0;
@@ -77,10 +100,6 @@ const bonus1 = 5;
 const bonus2 = 6;
 const quizz = 7;
 const retourDepart = 8;
-
-const tiles = [];
-
-
 
 // Map
 var gameMap = [
@@ -98,54 +117,49 @@ function initialise() {
   for (let y = 0; y < mapH; y++) {
     for (let x = 0; x < mapW; x++) {
 
-      let tileShape = new createjs.Shape();
       let image = new Image(tileW, tileH);
 
       switch (gameMap[((y * mapW) + x)]) {
         case start:
-          tileShape.graphics.beginFill('#bdbdbd').drawRect(0, 0, tileW, tileH);
+          image.src = "./assets/start.jpg";
           break;
 
         case intrus:
-          image.src = "./assets/intrus.png";
+          image.src = "./assets/intrus.jpg";
           break;
 
         case malus4:
-          image.src = "./assets/malus4.png";
+          image.src = "./assets/malus4.jpg";
           break;
 
         case malus2:
-          image.src = "./assets/malus2.png";
+          image.src = "./assets/malus2.jpg";
           break;
 
         case bonus1:
-          image.src = "./assets/bonus1.png";
+          image.src = "./assets/bonus1.jpg";
           break;
 
         case bonus2:
-          image.src = "./assets/bonus2.png";
+          image.src = "./assets/bonus2.jpg";
           break;
 
         case end:
-          tileShape.graphics.beginFill('#d8f500').drawRect(0, 0, tileW, tileH);
+          image.src = "./assets/end.jpg";
           break;
 
         case quizz:
-          image.src = "./assets/quizz.png";
+          image.src = "./assets/quizz.jpg";
           break;
 
         case retourDepart:
-          image.src = "./assets/death.png";
+          image.src = "./assets/restart.png";
           break;
-
 
         case normal:
         default:
-          tileShape.graphics.beginFill('#ccffcc').drawRect(0, 0, tileW, tileH);
-
-      }
-      tileShape.x = x * tileW;
-      tileShape.y = y * tileH;
+          image.src = "./assets/normal.jpg";
+      }  
 
       let tile = {
         type: gameMap[((y * mapW) + x)],
@@ -154,38 +168,33 @@ function initialise() {
       }
 
       let bitmap = new createjs.Bitmap(image);
-
       bitmap.x = x * tileW;
       bitmap.y = y * tileH;
+      bitmap.shadow = new createjs.Shadow("#000000", 5, 5, 10);
 
       tiles.push(tile);
 
-      scene.addChild(tileShape);
       scene.addChild(bitmap);
     }
   }
 
   // Ajout des pions sur la map
-  scene.addChild(j1Point);
-  scene.addChild(j2Point);
-  playersPoints.push(j1Point, j2Point);
+  scene.addChild(bitmapJ1);
+  scene.addChild(bitmapJ2);
+  playersPoints.push(bitmapJ1, bitmapJ2);
 
   if (players.length == 3) {
-    scene.addChild(j3Point);
-    playersPoints.push(j3Point);
+    scene.addChild(bitmapJ3);
+    playersPoints.push(bitmapJ3);
   }
 
   if (players.length == 4) {
-    scene.addChild(j3Point)
-    scene.addChild(j4Point);
-    playersPoints.push(j3Point, j4Point);
+    scene.addChild(bitmapJ3)
+    scene.addChild(bitmapJ4);
+    playersPoints.push(bitmapJ3, bitmapJ4);
   }
 
-  console.log(playersPoints)
-
-
-  setTimeout(() => { scene.update(); }, 200); // timeout pour garantir chargements des images avant affichage
-  //scene.update();
+  setTimeout(() => { scene.update(); }, 100); // timeout pour garantir chargements des images avant affichage
 }
 
 let buttonDice = document.getElementById('buttonDice');
@@ -198,68 +207,73 @@ buttonDice.addEventListener("click", function () {
 
   switch (actualPlayerId) {
     case 0:
-      j1Point.x += 100 * randomDice;
-      if ((j1Point.x > 1000 || j1Point.x == 930) && j1Point.y == 630) {
-        j1Point.x = 930;
-        j1Point.y = 630
+      //createjs.Tween.get(bitmapJ1).to({x: bitmapJ1.x + (100 * randomDice)}, 300, createjs.Ease.linear);
+      bitmapJ1.x += 100 * randomDice;    
+      if ((bitmapJ1.x > 1000 || bitmapJ1.x == 930) && bitmapJ1.y == 630) {
+        bitmapJ1.x = 930;
+        bitmapJ1.y = 630
         console.log(`${players[0].name} a gagné !`);
-      } else if (j1Point.x > 1000) {
-        j1Point.x -= 1000;
-        j1Point.y += 100;
+      } else if (bitmapJ1.x > 1000) {
+        bitmapJ1.x -= 1000;
+        bitmapJ1.y += 100;
       };
-      tileLandedX = Math.round(j1Point.x / 50) * 50;
-      tileLandedY = Math.round(j1Point.y / 50) * 50;
+      tileLandedX = Math.round(bitmapJ1.x / 50) * 50;
+      tileLandedY = Math.round(bitmapJ1.y / 50) * 50;
       break;
     case 1:
-      j2Point.x += 100 * randomDice;
-      if ((j2Point.x > 1000 || j2Point.x == 970) && j2Point.y == 630) {
-        j2Point.x = 970;
-        j2Point.y = 630
+      bitmapJ2.x += 100 * randomDice;
+      if ((bitmapJ2.x > 1000 || bitmapJ2.x == 970) && bitmapJ2.y == 630) {
+        bitmapJ2.x = 970;
+        bitmapJ2.y = 630
         console.log(`${players[1].name} a gagné !`);
-      } else if (j2Point.x > 1000) {
-        j2Point.x -= 1000;
-        j2Point.y += 100;
+      } else if (bitmapJ2.x > 1000) {
+        bitmapJ2.x -= 1000;
+        bitmapJ2.y += 100;
       }
-      tileLandedX = Math.round(j2Point.x / 50) * 50;
-      tileLandedY = Math.round(j2Point.y / 50) * 50;
+      tileLandedX = Math.round(bitmapJ2.x / 50) * 50;
+      tileLandedY = Math.round(bitmapJ2.y / 50) * 50;
       break;
     case 2:
-      j3Point.x += 100 * randomDice;
-      if ((j3Point.x > 1000 || j3Point.x == 930) && j3Point.y == 670) {
-        j3Point.x = 930;
-        j3Point.y = 670
+      bitmapJ3.x += 100 * randomDice;
+      if ((bitmapJ3.x > 1000 || bitmapJ3.x == 930) && bitmapJ3.y == 670) {
+        bitmapJ3.x = 930;
+        bitmapJ3.y = 670
         console.log(`${players[2].name} a gagné !`);
-      } else if (j3Point.x > 1000) {
-        j3Point.x -= 1000;
-        j3Point.y += 100;
+      } else if (bitmapJ3.x > 1000) {
+        bitmapJ3.x -= 1000;
+        bitmapJ3.y += 100;
       }
-      tileLandedX = Math.round(j3Point.x / 50) * 50;
-      tileLandedY = Math.round(j3Point.y / 50) * 50;
+      tileLandedX = Math.round(bitmapJ3.x / 50) * 50;
+      tileLandedY = Math.round(bitmapJ3.y / 50) * 50;
       break;
     case 3:
-      j4Point.x += 100 * randomDice;
-      if ((j4Point.x > 1000 || j4Point.x == 970) && j4Point.y == 670) {
-        j4Point.x = 970;
-        j4Point.y = 670
+      bitmapJ4.x += 100 * randomDice;
+      if ((bitmapJ4.x > 1000 || bitmapJ4.x == 970) && bitmapJ4.y == 670) {
+        bitmapJ4.x = 970;
+        bitmapJ4.y = 670
         console.log(`${players[3].name} a gagné !`);
-      } else if (j4Point.x > 1000) {
-        j4Point.x -= 1000;
-        j4Point.y += 100;
+      } else if (bitmapJ4.x > 1000) {
+        bitmapJ4.x -= 1000;
+        bitmapJ4.y += 100;
       }
-      tileLandedX = Math.round(j4Point.x / 50) * 50;
-      tileLandedY = Math.round(j4Point.y / 50) * 50;
+      tileLandedX = Math.round(bitmapJ4.x / 50) * 50;
+      tileLandedY = Math.round(bitmapJ4.y / 50) * 50;
       break;
     default:
       break;
   }
-  scene.update();
+  // Update the scene to move the player pawn
+  setTimeout(() => { scene.update(); }, 200);
 
+  // Get the tile where player landed
   const tileLanded = _.find(tiles, function(tile) { return tile.x == tileLandedX && tile.y == tileLandedY; });
 
+  // Execute the effect of the tile
   executeTile(tileLanded, actualPlayerId);
 
   diceDiv.innerHTML = randomDice + ' / ' + actualPlayerId;
-  setTimeout(() => { scene.update(); }, 500);
+  setTimeout(() => { scene.update(); }, 600);
+
   actualPlayerId++;
   if(actualPlayerId > _.last(players).id) {
     actualPlayerId = 0;
@@ -267,7 +281,6 @@ buttonDice.addEventListener("click", function () {
 });
 
 function executeTile(tile, actualPlayerId) {
-  console.log(tile);
 
   switch (tile.type) {
     case intrus:
